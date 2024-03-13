025e3293f73ae81cae5edb1b0449f13681a5e0bc
quarzasiphix <quarrysiphix@gmail.com>
2024-03-13 13:14:12 +0000

    made shortcode

--- quarza.php
+++ quarza.php
@@ -17,6 +17,8 @@ class quarza {
 
         // add assets(js, css)
         add_action('wp_enqueue_scripts', array($this, 'load_assets'));
+
+        add_shortcode('shortcode-test', array($this, 'load_shortcode'));
     }
 
     public function create_custom_post_type() {
@@ -59,6 +61,21 @@ class quarza {
         );
 
 
+    }
+
+    public function load_shortcode() 
+    {?>
+    <h!> test </h1>
+
+    <div class="quarza-form"> 
+    <form id="quarza-form-form">
+        <input type="email" placeholder="email">
+
+        <button class="btn btn-success btn-block"> Send message </button>
+    </form>
+    </div>
+
+    <?php //return 'shortcode works';
     }
 }
 
